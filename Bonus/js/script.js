f80f71434caa06180eab8715c7c6eaf680389530
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-02 20:07:28 +0100

    Feat: Snack 6

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -4,37 +4,37 @@
 // Snack 1
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-let numberInput = prompt("Insert a number");
+// let numberInput = prompt("Insert a number");
 
-if(numberInput % 2 === 0){
-    console.log(numberInput);
-}
-else{
-    numberInput++;
-    console.log(numberInput);
-}
+// if(numberInput % 2 === 0){
+//     console.log(numberInput);
+// }
+// else{
+//     numberInput++;
+//     console.log(numberInput);
+// }
 
 
 // Snack 2
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-const word1 = prompt("Insert your first word");
-const word2 = prompt("Insert your second word");
+// const word1 = prompt("Insert your first word");
+// const word2 = prompt("Insert your second word");
 
-let shortWord = "";
-let longWord = "";
+// let shortWord = "";
+// let longWord = "";
 
-if (word1.length > word2.length){
-    shortWord = word2;
-    longWord = word1;
-}
-else {
-    shortWord = word1;
-    longWord = word2;
-}
-console.log(shortWord);
-console.log(longWord);
+// if (word1.length > word2.length){
+//     shortWord = word2;
+//     longWord = word1;
+// }
+// else {
+//     shortWord = word1;
+//     longWord = word2;
+// }
+// console.log(shortWord);
+// console.log(longWord);
 
 
 // Snack 3
@@ -94,4 +94,44 @@ console.log(`The sum of all the numbers in ${numberStr} is equal to ${sum}`);;
 // ];
 
 // Snack 6
-// Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+// Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha 
+// meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+//            0  1  2  3  4  5  6  7  8  9
+let array1 = [4, 4, 4, 4, 6];
+//            0  1  2  3  4   
+let array2 = [1, 2, 3, 4, 5,7, 6, 8, 8, 9]
+let array1size = 0;
+let array2size = 0;
+
+let addElem = 0;
+
+for(let i = 0; i < array1.length; i++){
+    array1size = i;
+}
+console.log(`Snack 6: Size of array1 is ${array1size}`);
+
+for(let j = 0; j < array2.length; j++){
+    array2size = j;
+}
+console.log(`Snack 6: Size of array2 is ${array2size}`);
+
+
+if(array1size > array2size){
+    addElem = array1size - array2size;
+    console.log(`Snack 6: Array1 is bigger then Array2 by ${addElem} elemets`);
+
+    for(let k = 0; k < addElem; k++){
+        array2.push(Math.floor(Math.random() * 10));
+    }
+    console.log(`Snack 6: Initially shorter array2 is now bigger ${array2}`);
+}
+else{
+    addElem = array2size - array1size;
+    console.log(`Snack 6: Array2 is bigger then Array1 by ${addElem} elemets`);
+
+    for(let k = 0; k < addElem; k++){
+        array1.push(Math.floor(Math.random() * 10));
+    }
+    console.log(`Snack 6: Initially shorter array1 is now bigger ${array1}`);
+}
