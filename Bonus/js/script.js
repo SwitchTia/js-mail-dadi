0f3d3bdf2942331a42a1fd2f39ac81fce01f6c46
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-02 21:04:39 +0100

    Feat: Snack 4

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -4,28 +4,30 @@
 // Snack 1
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-let numberInput = prompt("Insert a number");
+let numberInput = prompt("Snack 1: Insert a number");
 
-if(numberInput % 2 === 0){
+if (numberInput % 2 === 0) {
     //console.log(numberInput);
 }
-else{
+else {
     numberInput++;
 }
 console.log(`Snack 1: The number is ${numberInput}`);
+
+
 
 
 // Snack 2
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-const word1 = prompt("Insert your first word");
-const word2 = prompt("Insert your second word");
+const word1 = prompt("Snack 2: Insert your first word");
+const word2 = prompt("Snack 2: Insert your second word");
 
 let shortWord = "";
 let longWord = "";
 
-if (word1.length > word2.length){
+if (word1.length > word2.length) {
     shortWord = word2;
     longWord = word1;
 }
@@ -35,6 +37,8 @@ else {
 }
 console.log(`Snack 2: The short word is ${shortWord}`);
 console.log(`Snack 2: The long word is ${longWord}`);
+
+
 
 
 // Snack 3
@@ -52,20 +56,39 @@ console.log(`Snack 3: The string array is ${numberStrArray}`);
 //let sum = numberArray.reduce((a, b)=> a + b, 0);
 let sum = 0;
 
-for(let i = 0; i < numberStrArray.length; i++){
+for (let i = 0; i < numberStrArray.length; i++) {
     let currentNumberStr = numberStrArray[i];
     let currentNumber = parseInt(currentNumberStr);
-    console.log(currentNumber);
+    //console.log(currentNumber);
     sum += currentNumber;
 }
 console.log(`Snack 3: The sum of all the numbers in ${numberStr} is equal to ${sum}`);;
+
+
 
 // Snack 4
 // Stampa il cubo dei primi N numeri,
 // dove N è un numero indicato dall’utente.
 
+let inputNumber = prompt("Snack 4: Insert a number please:");
+let n = parseInt(inputNumber);
+let arrayN = [];
+
+for(let i = 1; i < n; i++){
+    arrayN.push(i);
+}
+console.log(arrayN);
+
+for(let i = 0; i < arrayN.length; i++){
+    let currentNumber = arrayN[i];
+    console.log(`Snack 4:  ${currentNumber**3}`);
+}
+
+
+
 // Snack 5
-// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di 5 invitati con nome e cognome.
+// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+// e da queste vuole generare una falsa lista di 5 invitati con nome e cognome.
 
 // const nomi = [
 //   "Albert",
@@ -100,38 +123,55 @@ console.log(`Snack 3: The sum of all the numbers in ${numberStr} is equal to ${s
 //            0  1  2  3  4  5  6  7  8  9
 let array1 = [4, 4, 4, 4, 6];
 //            0  1  2  3  4   
-let array2 = [1, 2, 3, 4, 5,7, 6, 8, 8, 9]
+let array2 = [1, 2, 3, 4, 5, 7, 6, 8, 8, 9]
+
 let array1size = 0;
 let array2size = 0;
 
 let addElem = 0;
 
-for(let i = 0; i < array1.length; i++){
+for (let i = 0; i < array1.length; i++) {
     array1size = i;
 }
 console.log(`Snack 6: Size of array1 is ${array1size}`);
 
-for(let j = 0; j < array2.length; j++){
+for (let j = 0; j < array2.length; j++) {
     array2size = j;
 }
 console.log(`Snack 6: Size of array2 is ${array2size}`);
 
 
-if(array1size > array2size){
+if (array1size > array2size) {
     addElem = array1size - array2size;
     console.log(`Snack 6: Array1 is bigger then Array2 by ${addElem} elemets`);
 
-    for(let k = 0; k < addElem; k++){
+    for (let k = 0; k < addElem; k++) {
         array2.push(Math.floor(Math.random() * 10));
     }
     console.log(`Snack 6: Initially shorter array2 is now bigger ${array2}`);
 }
-else{
+else {
     addElem = array2size - array1size;
     console.log(`Snack 6: Array2 is bigger then Array1 by ${addElem} elemets`);
 
-    for(let k = 0; k < addElem; k++){
+    for (let k = 0; k < addElem; k++) {
         array1.push(Math.floor(Math.random() * 10));
     }
     console.log(`Snack 6: Initially shorter array1 is now bigger ${array1}`);
 }
+
+// for (let k = 0; k < addElem; k++) {
+//     if (array1size > array2size) {
+//         addElem = array1size - array2size;
+//         console.log(`Snack 6: Array1 is bigger then Array2 by ${addElem} elemets`);
+
+//         array2.push(Math.floor(Math.random() * 10));
+//         console.log(`Snack 6: Initially shorter array2 is now bigger ${array2}`);
+//     }
+//     else{
+//         console.log(`Snack 6: Array2 is bigger then Array1 by ${addElem} elemets`);
+
+//         array1.push(Math.floor(Math.random() * 10));
+//         console.log(`Snack 6: Initially shorter array1 is now bigger ${array1}`)
+//     }
+// }
